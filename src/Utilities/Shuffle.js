e861c1928a02355311a9a6d670c2b1010d22882a
mwstarbuck <mwstarbuck@gmail.com>
2021-11-01 18:13:10 -0500

    FRUpkeep
    added feature to shuffl set of cards

--- src/Utilities/Shuffle.js
+++ src/Utilities/Shuffle.js
@@ -1,8 +1,18 @@
+import React from 'react';
 
-function shuffle(min, max) {
+function getRandom(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    console.log(Math.floor(Math.random(min, max) * (max - min + 1) + min));
+    return Math.floor(Math.random(min, max) * (max - min + 1) + min);
 }
 
-shuffle(0, 15);
+//shuffle(0, 14);
+export function shuffle(set, shuffledCards) {
+    let len = set.length;
+    for (let i = 0; i < len; i++) {
+
+        let r = getRandom(0, set.length - 1);
+        shuffledCards.push(set[r]);
+        set.splice(r, 1);
+    }   
+}
