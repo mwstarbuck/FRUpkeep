03430b3a3090aad6f730ba5d4b24a11279cfb84b
mwstarbuck <mwstarbuck@gmail.com>
2021-11-15 15:21:56 -0600

    added container functions amd state

--- src/Containers/GameContainer.ts
+++ src/Containers/GameContainer.ts
@@ -1,11 +1,33 @@
 import React from 'react';
+import globalHook, {Store} from 'use-global-hook';
 
 export interface GameState {
-    showSetup: boolean,
-    showGameplay: boolean
+    showSetup?: boolean,
+    showGameplay?: boolean
 }
 
 export interface GameActions {
-    setShowSetup: () => void,
-    setShowGameplay: () => void
+    setShowSetup: (value: any) => void,
+    setShowGameplay: (value: any) => void
 }
+
+const initialState: GameState = {
+    showSetup: false,
+    showGameplay: false
+}
+
+const setShowSetup = (store: Store<GameState, GameActions>, value: boolean) => {
+    store.setState({showSetup: value});
+}
+
+const setShowGameplay = (store: Store<GameState, GameActions>, value: boolean) => {
+    store.setState({showGameplay: value});
+}
+
+const actions = {
+    setShowSetup,
+    setShowGameplay
+}
+
+const useGameContainer = globalHook<GameState, GameActions>(React, initialState, actions);
+export default useGameContainer;
