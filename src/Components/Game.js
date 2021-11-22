74bcafa39b6de8834639a73d7bf68c7f77195c1c
mwstarbuck <mwstarbuck@gmail.com>
2021-11-22 11:34:06 -0600

    using map to create teams grid, add text helper

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -6,24 +6,25 @@ import useGameContainer from '../Containers/GameContainer';
 
 
 const Game = () => {
-    // const [showSetup, setShowSetup] = React.useState(false);
-    const [showGamePlay, setShowGameplay] = useGameContainer(state => state.showGameplay, actions => actions.setShowGameplay);
-    const [showSetup, setShowSetup] = useGameContainer(state => state.showSetup, actions => actions.setShowSetup);
-    // const [showGameplay] = useGameContainer(state => state.showGameplay, actions => null);
-    return (
-        <div>
-            {
-                !showSetup ?
-                    <Welcome setShowSetup={setShowSetup} />
-                    : !showGamePlay ? <Setup
-                        setShowSetup={setShowSetup}
-                        setShowGamePlay={setShowGameplay} />
-                        : <GamePlay
-                            setShowSetup={setShowSetup}
-                            setShowGamePlay={setShowGameplay} />
-            }
-        </div>
-    );
+  // const [showSetup, setShowSetup] = React.useState(false);
+  const [showGamePlay, setShowGameplay] = useGameContainer(state => state.showGameplay, actions => actions.setShowGameplay);
+  const [showSetup, setShowSetup] = useGameContainer(state => state.showSetup, actions => actions.setShowSetup);
+  // const [showGameplay] = useGameContainer(state => state.showGameplay, actions => null);
+
+  return (
+    <div>
+      {
+        !showSetup ?
+          <Welcome setShowSetup={setShowSetup} />
+          : !showGamePlay ? <Setup
+            setShowSetup={setShowSetup}
+            setShowGamePlay={setShowGameplay} />
+            : <GamePlay
+              setShowSetup={setShowSetup}
+              setShowGamePlay={setShowGameplay} />
+      }
+    </div>
+  );
 }
 
 export default Game;
--- src/Components/GamePlay.js
+++ src/Components/GamePlay.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Button, Grid } from '@material-ui/core';
 import useGameContainer from '../Containers/GameContainer';
+import { capitalize } from '../Utilities/TextHelper';
 
 
 const GamePlay = (props) => {
   const { setShowSetup, setShowGamePlay } = props;
   const [teams] = useGameContainer(state => state.computerTeams, actions => null);
+
   return (
     <div>
       <div>This is the Game Play Screen</div>
       <Grid container >
         <Grid container>
-          <Grid item sm={6} style={{ backgroundColor: teams[0].color, color: teams[0].color === 'White' ? 'black' : 'white' }}>
-            <h1>{teams[0].type} team </h1>
-            <h2>Cards remaining: {teams[0].rouleurDeck.length}</h2>
-          </Grid>
-          {/* <Grid item sm={6} style={{ backgroundColor: teams[1].color, color: teams[1].color === 'White' ? 'black' : 'white' }}>
-            <h1>{teams[1].type} team # {teams[1].number} </h1>
-            <h2>Cards remaining: {teams[1].rouleurDeck.length}</h2>
-          </Grid> */}
+          {
+            teams.map((team, index) => {
+              return (
+                <Grid item sm={6} style={{ backgroundColor: team.color, color: team.color === 'White' ? 'black' : 'white' }}>
+                  <h1>{team.color} {capitalize(team.type)} Team </h1>
+                  <h2>Cards remaining: {team.rouleurDeck.length}</h2>
+                </Grid>
+              )
+            })
+          }
         </Grid>
       </Grid>
       <Button
