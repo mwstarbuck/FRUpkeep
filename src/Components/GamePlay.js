5f8dc40787b759773139cc5e4c2ae00ea5bf8f8c
mwstarbuck <mwstarbuck@gmail.com>
2021-11-23 12:27:55 -0600

    Place holder for Decks and discards

--- src/Components/GamePlay.js
+++ src/Components/GamePlay.js
@@ -4,6 +4,7 @@ import useGameContainer from '../Containers/GameContainer';
 import { capitalize } from '../Utilities/TextHelper';
 
 
+
 const GamePlay = (props) => {
   const { setShowSetup, setShowGamePlay } = props;
   const [teams] = useGameContainer(state => state.computerTeams, actions => null);
@@ -16,9 +17,15 @@ const GamePlay = (props) => {
           {
             teams.map((team, index) => {
               return (
-                <Grid item sm={6} style={{ backgroundColor: team.color, color: team.color === 'White' ? 'black' : 'white' }}>
-                  <h1>{team.color} {capitalize(team.type)} Team </h1>
-                  <h2>Cards remaining: {team.rouleurDeck.length}</h2>
+                <Grid item sm={6} style={{ /*backgroundImage: 'url(/images/black.png)'*/ backgroundColor: team.color, color: team.color === 'White' ? 'black' : 'white' }}>
+                  <h2>{team.color} {capitalize(team.type)} Team </h2>
+                  <h3>Cards remaining: {team.rouleurDeck.length}</h3>
+                  <Grid item sm={12}>
+                    <div style={{ display: 'inline-flex' }}>
+                      <div style={{ height: 150 + 'px', width: 100 + 'px', display: 'inline-block', margin: 10 + 'px', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}`, borderRadius: 5 + 'px' }}><h5>Deck</h5> <h3>{team.rouleurDeck[0].movement}</h3></div>
+                      <div style={{ height: 150 + 'px', width: 100 + 'px', display: 'inline-block', margin: 10 + 'px', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}`, borderRadius: 5 + 'px' }}><h5>Discard Pile</h5><h3></h3></div>
+                    </div>
+                  </Grid>
                 </Grid>
               )
             })
