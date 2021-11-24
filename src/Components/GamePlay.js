c27edb1fbb6afe5cde959cbc7fbd60833c445ee0
mwstarbuck <mwstarbuck@gmail.com>
2021-11-24 15:50:20 -0600

    adding room for sprinter cards display

--- src/Components/GamePlay.js
+++ src/Components/GamePlay.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Grid } from '@material-ui/core';
 import useGameContainer from '../Containers/GameContainer';
 import { capitalize } from '../Utilities/TextHelper';
+import black from '../img/black.png';
 
 
 
@@ -12,25 +13,36 @@ const GamePlay = (props) => {
   return (
     <div>
       <div>This is the Game Play Screen</div>
-      <Grid container >
-        <Grid container>
-          {
-            teams.map((team, index) => {
-              return (
-                <Grid item sm={6} style={{ /*backgroundImage: 'url(/images/black.png)'*/ backgroundColor: team.color, color: team.color === 'White' ? 'black' : 'white' }}>
-                  <h2>{team.color} {capitalize(team.type)} Team </h2>
-                  <h3>Cards remaining: {team.rouleurDeck.length}</h3>
-                  <Grid item sm={12}>
-                    <div style={{ display: 'inline-flex' }}>
-                      <div style={{ height: 150 + 'px', width: 100 + 'px', display: 'inline-block', margin: 10 + 'px', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}`, borderRadius: 5 + 'px' }}><h5>Deck</h5> <h3>{team.rouleurDeck[0].movement}</h3></div>
-                      <div style={{ height: 150 + 'px', width: 100 + 'px', display: 'inline-block', margin: 10 + 'px', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}`, borderRadius: 5 + 'px' }}><h5>Discard Pile</h5><h3></h3></div>
+      <Grid container>
+        {
+          teams.map((team, index) => {
+            return (
+              <Grid item sm={6} style={{ /*backgroundImage: 'url(/images/black.png)'*/ backgroundColor: team.color, color: team.color === 'White' ? 'black' : 'white' }}>
+                <Grid container>
+                  <Grid item sm={4}>
+                    <div style={{ height: 100 + '%', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}` }}>
+                      <h2>{team.color} {capitalize(team.type)} Team </h2>
+                      <h3>Cards remaining: {team.rouleurDeck.length}</h3>
+                      {/* <img style={{ width: 200 + 'px' }} src={black} /> */}
+                    </div>
+                  </Grid>
+                  <Grid item sm={4}>
+                    <div style={{ width: '100%', display: 'inline-flex', justifyContent: 'center', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}` }}>
+                      <div style={{ height: 150 + 'px', width: 100 + 'px', margin: 10 + 'px', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}`, borderRadius: 5 + 'px' }}><h5>Deck</h5> <h3>{team.rouleurDeck[0].movement}</h3></div>
+                      <div style={{ height: 150 + 'px', width: 100 + 'px', margin: 10 + 'px', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}`, borderRadius: 5 + 'px' }}><h5>Discard Pile</h5><h3></h3></div>
+                    </div>
+                  </Grid>
+                  <Grid item sm={4}>
+                    <div style={{ width: '100%', display: 'inline-flex', justifyContent: 'center', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}` }}>
+                      <div style={{ height: 150 + 'px', width: 100 + 'px', margin: 10 + 'px', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}`, borderRadius: 5 + 'px' }}><h5>Deck</h5> <h3>{team.rouleurDeck[0].movement}</h3></div>
+                      <div style={{ height: 150 + 'px', width: 100 + 'px', margin: 10 + 'px', border: 2 + `px solid ${team.color == 'White' ? 'black' : 'white'}`, borderRadius: 5 + 'px' }}><h5>Discard Pile</h5><h3></h3></div>
                     </div>
                   </Grid>
                 </Grid>
-              )
-            })
-          }
-        </Grid>
+              </Grid>
+            )
+          })
+        }
       </Grid>
       <Button
         variant="outlined"
